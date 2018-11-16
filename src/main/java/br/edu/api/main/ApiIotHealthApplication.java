de0e04f8e684bb9a405fe654efe2938b4f3aa2f2
Gabriel Martins <ga.smartins94@gmail.com>
2018-11-15 22:16:51 -0200

    Ajustes Diretório Views

--- src/main/java/br/edu/api/main/ApiIotHealthApplication.java
+++ src/main/java/br/edu/api/main/ApiIotHealthApplication.java
@@ -2,10 +2,17 @@ package br.edu.api.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiIotHealthApplication {
+public class ApiIotHealthApplication extends SpringBootServletInitializer { 
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiIotHealthApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiIotHealthApplication.class, args);
 	}
