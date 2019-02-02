bbdcc4834fc0f082ecf8f1e4a908cf26cad8341a
Gabriel Martins <ga.smartins94@gmail.com>
2019-02-02 20:49:03 -0200

    Configuracao H2 proporties

--- src/main/java/br/edu/api/app/ApiIotHealthApplication.java
+++ src/main/java/br/edu/api/app/ApiIotHealthApplication.java
@@ -1,11 +1,13 @@
 package br.edu.api.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ApiIotHealthApplication extends SpringBootServletInitializer { 
 
 	@Override
