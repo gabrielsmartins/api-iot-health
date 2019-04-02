833e6537d0af803117e066b2c164a14766e22b08
Gabriel Martins <ga.smartins94@gmail.com>
2019-04-01 21:49:33 -0300

    Ajustes Swagger

--- src/main/java/br/edu/api/iot/app/config/SwaggerConfiguration.java
+++ src/main/java/br/edu/api/iot/app/config/SwaggerConfiguration.java
@@ -2,8 +2,6 @@ package br.edu.api.iot.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -15,32 +13,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration extends WebMvcConfigurationSupport {
-	
+public class SwaggerConfiguration {
+
 	@Bean
-	public Docket api() {                
-	    return new Docket(DocumentationType.SWAGGER_2)          
-	      .select()
-	      .apis(RequestHandlerSelectors.basePackage("br.edu.api.iot.controller"))
-	      .paths(PathSelectors.any())
-	      .build()
-	      .apiInfo(apiInfo());
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("br.edu.api.iot.controller"))
+				 .paths(PathSelectors.ant("/**"))
+				.build().apiInfo(apiInfo());
 	}
-	 
+
 	private ApiInfo apiInfo() {
-	   return new ApiInfoBuilder()
-	    .title("Api IoT Health")
-	    .version("1.0.0")
-	    .description("Api para armazenamento de dados de frequência cardíaca, oxigenação e temperatura")
-	    .build();
+		return new ApiInfoBuilder().title("Api IoT Health").version("1.0.0")
+				.description("Api para armazenamento de dados de frequência cardíaca, oxigenação e temperatura")
+				.build();
 	}
-	
-	 @Override
-	    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-	        registry.addResourceHandler("swagger-ui.html")
-	                .addResourceLocations("classpath:/META-INF/resources/");
-	        registry.addResourceHandler("/webjars/**")
-	                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-	    }
 
 }
