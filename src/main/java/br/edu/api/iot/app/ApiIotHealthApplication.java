633d0e0372eeeda10694879e655b5544dbabfb42
Gabriel Martins <ga.smartins94@gmail.com>
2019-04-03 22:40:30 -0300

    Reset Main Class

--- src/main/java/br/edu/api/iot/app/ApiIotHealthApplication.java
+++ src/main/java/br/edu/api/iot/app/ApiIotHealthApplication.java
@@ -3,8 +3,6 @@ package br.edu.api.iot.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -14,14 +12,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages= {"br.edu.api.iot.entity"})
 @EnableJpaRepositories(basePackages= {"br.edu.api.iot.repository"})
 @EnableCaching
-public class ApiIotHealthApplication extends SpringBootServletInitializer  { 
+public class ApiIotHealthApplication   { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiIotHealthApplication.class, args);
 	}
 
-	 @Override
-	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application){ 
-	   return application.sources(ApiIotHealthApplication.class); 
-	   }
+	 
 }
